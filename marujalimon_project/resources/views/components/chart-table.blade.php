1983e70da23bda58221954a8cb0456b4ca6f41d7
Aarón Freire <freiresousa97@gmail.com>
2024-04-24 10:48:21 +0200

    arreglamos multiseleccion y admin

--- marujalimon_project/resources/views/components/chart-table.blade.php
+++ marujalimon_project/resources/views/components/chart-table.blade.php
@@ -7,32 +7,35 @@
 <!-- Tarjetas de voluntarios con nuevo estilo -->
 <div id="cardView" class="row row-cols-md-3 row-cols-lg-4 gx-1 mt-5">
     @foreach ($voluntarios as $voluntario)
-    <div class="col mb-4">
-        <!-- Tarjeta con nuevo estilo -->
-        <div class="card h-100 border-0 shadow-sm" data-tilt>
-            <!-- Imagen de perfil del voluntario -->
-            <img src="{{ $voluntario->imagenPerfil
+        <div class="col mb-4">
+            <!-- Tarjeta con nuevo estilo -->
+            <div class="card h-100 border-0 shadow-sm" data-tilt>
+                <!-- Imagen de perfil del voluntario -->
+                <img src="{{ $voluntario->imagenPerfil
                     ? $voluntario->imagenPerfil->IMG_path
                     : 'data:image/svg+xml,%3Csvg
-                                                                                        xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 448 512\'%3E%3Cpath
-                                                                                        fill=\'%23999\' d=\'M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96
-                                                                                        57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.4-46.6 16-72.9
-                                                                                        16s-50.7-5.6-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48
-                                                                                        48 48h352c26.5 0 48-21.5
-                                                                                        48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z\'/%3E%3C/svg%3E' }}" class="card-img-top" alt="Imagen de perfil del voluntario">
+                                                                                                                                                                        xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 448 512\'%3E%3Cpath
+                                                                                                                                                                        fill=\'%23999\' d=\'M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96
+                                                                                                                                                                        57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.4-46.6 16-72.9
+                                                                                                                                                                        16s-50.7-5.6-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48
+                                                                                                                                                                        48 48h352c26.5 0 48-21.5
+                                                                                                                                                                        48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z\'/%3E%3C/svg%3E' }}"
+                    class="card-img-top" alt="Imagen de perfil del voluntario">
 
-            <div class="card-body">
-                <h5 class="card-title">
-                    <i class="fas fa-user"></i> {{ $voluntario->VOL_nombre }} {{ $voluntario->VOL_apellidos }}
-                </h5>
-                <p class="card-text">
-                    <i class="fas fa-id-card"></i> DNI: {{ $voluntario->VOL_dni }}
-                </p>
-                <a href="{{ route('voluntarios.show', ['voluntario' => $voluntario]) }}" class="btn btn-primary" style="margin-right: 12px;">Más información</a>
-                <a href="{{ route('voluntario.edit_form', ['voluntario' => $voluntario]) }}" class="btn btn-primary ">Modificar</a>
+                <div class="card-body">
+                    <h5 class="card-title">
+                        <i class="fas fa-user"></i> {{ $voluntario->VOL_nombre }} {{ $voluntario->VOL_apellidos }}
+                    </h5>
+                    <p class="card-text">
+                        <i class="fas fa-id-card"></i> DNI: {{ $voluntario->VOL_dni }}
+                    </p>
+                    <a href="{{ route('voluntarios.show', ['voluntario' => $voluntario]) }}" class="btn btn-primary"
+                        style="margin-right: 12px;">Más información</a>
+                    <a href="{{ route('voluntario.edit_form', ['voluntario' => $voluntario]) }}"
+                        class="btn btn-primary ">Modificar</a>
+                </div>
             </div>
         </div>
-    </div>
     @endforeach
 </div>
 
@@ -42,10 +45,17 @@
     </div>
 @endif
 
+@if (session('error'))
+    <div class="alert alert-danger">
+        {{ session('error') }}
+    </div>
+@endif
+
+
 <div id="addHoursForm">
     <form action="{{ route('horas.añadir') }}" method="POST">
         @csrf
-        <input type="hidden" name="voluntarioId" id="voluntarioId" value="">
+        <input type="hidden" name="voluntariosSeleccionados" id="voluntariosSeleccionados" value="">
 
         <label for="horas">Horas:</label>
         <input type="number" name="horas" id="horas" required>
@@ -53,7 +63,7 @@
         <label for="tarea_id">Tarea:</label>
         <select name="tarea_id" id="tarea_id" required>
             @foreach ($tareas as $tarea)
-            <option value="{{ $tarea->TAR_id }}">{{ $tarea->TAR_nombre }}</option>
+                <option value="{{ $tarea->TAR_id }}">{{ $tarea->TAR_nombre }}</option>
             @endforeach
         </select>
 
@@ -81,18 +91,18 @@
             </thead>
             <tbody>
                 @foreach ($voluntarios_all as $voluntario)
-                <tr>
-                    <td>{{ $voluntario->VOL_id }}</td>
-                    <td>{{ $voluntario->VOL_nombre }}</td>
-                    <td>{{ $voluntario->VOL_apellidos }}</td>
-                    <td>{{ $voluntario->VOL_dni }}</td>
-                    <td>{{ $voluntario->VOL_fecha_nac }}</td>
-                    <td>{{ $voluntario->VOL_domicilio }}</td>
-                    <td>{{ $voluntario->VOL_cp }}</td>
-                    <td>{{ $voluntario->VOL_tel1 }}</td>
-                    <td>{{ $voluntario->VOL_sexo }}</td>
-                    <td>{{ $voluntario->VOL_mail }}</td>
-                </tr>
+                    <tr>
+                        <td>{{ $voluntario->VOL_id }}</td>
+                        <td>{{ $voluntario->VOL_nombre }}</td>
+                        <td>{{ $voluntario->VOL_apellidos }}</td>
+                        <td>{{ $voluntario->VOL_dni }}</td>
+                        <td>{{ $voluntario->VOL_fecha_nac }}</td>
+                        <td>{{ $voluntario->VOL_domicilio }}</td>
+                        <td>{{ $voluntario->VOL_cp }}</td>
+                        <td>{{ $voluntario->VOL_tel1 }}</td>
+                        <td>{{ $voluntario->VOL_sexo }}</td>
+                        <td>{{ $voluntario->VOL_mail }}</td>
+                    </tr>
                 @endforeach
             </tbody>
         </table>
@@ -113,15 +123,34 @@
 
     let table = $('#data-table-default').DataTable({
         responsive: true,
-        select: true,
+        select: {
+            style: 'multi'
+        }
     });
+
+    // Array para almacenar las IDs de los voluntarios seleccionados
+    let voluntariosSeleccionados = [];
 
     // Agregar evento de escucha para la selección de filas
     $('#data-table-default tbody').on('click', 'tr', function() {
+        // Obtener la ID del voluntario seleccionado
+        let voluntarioId = table.row(this).data()[0];
 
-        console.log("Voluntario seleccionado: " + voluntarioId);
-        $('#voluntarioId').val(table.row(this).data()[0]);
+        // Verificar si la ID del voluntario ya está en el array
+        let index = voluntariosSeleccionados.indexOf(voluntarioId);
 
+        // Si la ID no está en el array, agregarla
+        if (index === -1) {
+            voluntariosSeleccionados.push(voluntarioId);
+        }
+
+        // Imprimir el array de IDs de voluntarios seleccionados
+        console.log("Voluntarios seleccionados:", voluntariosSeleccionados);
+
+        // Puedes hacer lo que necesites con el array aquí
+        // Por ejemplo, si quieres almacenarlo en un campo oculto como una cadena JSON
+        $('#voluntariosSeleccionados').val(JSON.stringify(voluntariosSeleccionados));
+        console.log($('#voluntariosSeleccionados').val());
     });
 </script>
 <script src="{{ asset('js/card-table/switchCardTable.js') }}"></script>
