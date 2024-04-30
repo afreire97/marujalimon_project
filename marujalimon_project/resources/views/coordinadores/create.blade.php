e0317348d40d258163f76ef3247c11332fd0e116
gonzgimnzjavier <javigongimenez@gmail.com>
2024-04-30 17:37:57 +0200

    cambios en los formularios

--- marujalimon_project/resources/views/coordinadores/create.blade.php
+++ marujalimon_project/resources/views/coordinadores/create.blade.php
@@ -35,7 +35,7 @@
         </div>
         @endif
 
-        <form action="{{ route('coordinador.store') }}" method="POST" enctype="multipart/form-data">
+        <form action="{{ route('coordinadores.store') }}" method="POST" enctype="multipart/form-data">
             @csrf
 
             <div class="card-body">
