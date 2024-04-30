c509b5012f0bd1ca17cd252cebc0ee7b3527bd82
gonzgimnzjavier <javigongimenez@gmail.com>
2024-04-30 17:25:49 +0200

    CAMBIOS EN EL FORMULARIO DE REGISTRO DE VOLUNTARIOS

--- marujalimon_project/resources/views/coordinadores/create.blade.php
+++ marujalimon_project/resources/views/coordinadores/create.blade.php
@@ -13,16 +13,41 @@
     <form action="{{ route('coordinador.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
 
-        <div class="card-body">
-            <!-- Campos del formulario -->
-            @foreach ($fields as $name => $label)
+        <div class="container mt-5 mb-5">
+    <div class="card shadow">
+        <div class="card-header text-white text-center">
+            <h3>Registrar Coordinador</h3>
+        </div>
+        <!-- Mensajes de éxito y de error -->
+        @if(session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+        @endif
+
+        @if($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
+
+        <form action="{{ route('coordinador.store') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+
+            <div class="card-body">
+                <!-- Campos del formulario -->
+                @foreach ($fields as $name => $label)
                 <div class="mb-3 row">
                     <label class="col-md-4 col-form-label text-md-end">
                         {{ $label }} @if ($name !== 'COO_tel1' && $name !== 'COO_cp' && $name !== 'COO_sexo') <span class="text-danger">*</span> @endif
                     </label>
                     <div class="col-md-6">
                         @if ($name === 'COO_fecha_nac')
-                            <input type="date" name="{{ $name }}" class="form-control">
+                            <input type="date" name="{{ $name }}" class="form-control" required>
                         @elseif ($name === 'COO_tel1')
                             <input type="text" name="{{ $name }}" class="form-control">
                         @elseif ($name === 'COO_cp')
@@ -34,54 +59,58 @@
                                 @endforeach
                             </select>
                         @else
-                            <input type="text" name="{{ $name }}" class="form-control" @if ($name !== 'COO_tel1' && $name !== 'COO_cp' && $name !== 'COO_sexo') required @endif>
+                            <input type="text" name="{{ $name }}" class="form-control" required>
                         @endif
                     </div>
                 </div>
-            @endforeach
+                @endforeach
 
-            <!-- Campo para seleccionar la delegación -->
-            <div class="mb-3 row">
-                <label class="col-md-4 col-form-label text-md-end">Delegación</label>
-                <div class="col-md-6">
-                    <select name="delegacion_id" class="form-select">
-                        <option value="" selected disabled>Selecciona una delegación</option>
-                        @foreach ($delegaciones as $delegacion)
-                            <option value="{{ $delegacion->DEL_id }}">{{ $delegacion->DEL_nombre }}</option>
-                        @endforeach
-                    </select>
+                <!-- Campo para seleccionar la delegación -->
+                <div class="mb-3 row">
+                    <label class="col-md-4 col-form-label text-md-end">Delegación</label>
+                    <div class="col-md-6">
+                        <select name="delegacion_id" class="form-select" required>
+                            <option value="" selected disabled>Selecciona una delegación</option>
+                            @foreach ($delegaciones as $delegacion)
+                                <option value="{{ $delegacion->DEL_id }}">{{ $delegacion->DEL_nombre }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+
+                <!-- Campo para la imagen de perfil -->
+                <div class="mb-3 row">
+                    <label class="col-md-4 col-form-label text-md-end">Imagen de Perfil <span class="text-danger">*</span></label>
+                    <div class="col-md-6">
+                        <input type="file" name="imagen_perfil" class="form-control" accept="image/*" required>
+                    </div>
+                </div>
+
+                <!-- Campos para contraseña -->
+                <div class="mb-3 row">
+                    <label class="col-md-4 col-form-label text-md-end">Contraseña<span class="text-danger">*</span></label>
+                    <div class="col-md-6">
+                        <input type="password" class="form-control fs-13px" placeholder="Contraseña" name="password" required autocomplete="new-password" />
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label class="col-md-4 col-form-label text-md-end">Confirmar Contraseña <span class="text-danger">*</span></label>
+                    <div class="col-md-6">
+                        <input type="password" class="form-control fs-13px" placeholder="Confirmar Contraseña" name="password_confirmation" required autocomplete="new-password" />
+                        <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                    </div>
                 </div>
             </div>
 
-            <!-- Campo para la imagen de perfil -->
-            <div class="mb-3 row">
-                <label class="col-md-4 col-form-label text-md-end">
-                    Imagen de Perfil <span class="text-danger">*</span>
-                </label>
-                <div class="col-md-6">
-                    <input type="file" name="imagen_perfil" class="form-control" accept="image/*" required>
-                </div>
+            <div class="card-footer text-end">
+                <a href="{{ url()->previous() }}" class="btn btn-secondary">Volver</a>
+                <button type="submit" class="btn btn-primary">Registrar</button>
             </div>
-            <div class="mb-3">
-                <label class="mb-2 col-md-4 col-form-label text-md-end" for="password">Password <span class="text-danger">*</span></label>
-                <div class="col-md-6">
-                    <input type="password" class="form-control fs-13px" placeholder="Password" name="password" required autocomplete="new-password" />
-                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
-                </div>
-            </div>
+        </form>
+    </div>
+</div>
 
-            <div class="mb-3">
-                <label class="mb-2 col-md-4 col-form-label text-md-end" for="password_confirmation">Confirm Password <span class="text-danger">*</span></label>
-                <div class="col-md-6">
-                    <input type="password" class="form-control fs-13px" placeholder="Confirm Password" name="password_confirmation" required autocomplete="new-password" />
-                    <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-                </div>
-        </div>
-
-        <div class="card-footer text-end">
-            <a href="{{ url()->previous() }}" class="btn btn-secondary">Volver</a>
-            <button type="submit" class="btn btn-primary">Registrar</button>
-        </div>
     </form>
 
 </x-layout>
--- marujalimon_project/resources/views/voluntarios/crear_voluntario_form.blade.php
+++ marujalimon_project/resources/views/voluntarios/crear_voluntario_form.blade.php
@@ -5,139 +5,142 @@
 
     </head>
     <div class="container mt-5 mb-5">
-        <div class="card shadow">
-            <div class="card-header text-white text-center">
-                <h3>Registrar Voluntario</h3>
-            </div>
-            <!-- Mensajes de éxito -->
-            @if(session('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
-            </div>
-            @endif
-
-            <!-- Mensajes de error de validación -->
-            @if($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-            @endif
-
-            <form action="{{ route('storeVoluntario') }}" method="POST" enctype="multipart/form-data">
-                @csrf <!-- Directiva Blade para el token CSRF -->
-                <div class="card-body">
-                    <!-- Campos del formulario -->
-                    @php
-                    $fields = [
-                    'VOL_nombre' => 'Nombre',
-                    'VOL_apellidos' => 'Apellidos',
-                    'VOL_dni' => 'DNI',
-                    'VOL_fecha_nac' => 'Fecha de Nacimiento',
-                    'VOL_domicilio' => 'Domicilio',
-                    'VOL_cp' => 'Código Postal',
-                    'VOL_tel1' => 'Teléfono',
-                    'VOL_sexo' => 'Sexo',
-                    'VOL_mail' => 'Correo Electrónico'
-                    ];
-                    $selectFields = [
-                    'VOL_sexo' => ['Masculino', 'Femenino', 'Otro']
-                    ];
-                    @endphp
-
-                    @foreach ($fields as $name => $label)
-                    <div class="mb-3 row">
-                        <label class="col-md-4 col-form-label text-md-end">
-                            {{ $label }} <span class="text-danger">*</span>
-                        </label>
-                        <div class="col-md-6">
-                            @if (array_key_exists($name, $selectFields))
-                            <select name="{{ $name }}" class="form-select" required>
-                                <option value="" selected disabled>Selecciona una opción</option>
-                                @foreach ($selectFields[$name] as $option)
-                                <option value="{{ $option }}">{{ $option }}</option>
-                                @endforeach
-                            </select>
-                            @elseif($name == 'VOL_fecha_nac')
-                            <input type="date" name="{{ $name }}" class="form-control" required>
-                            @else
-                            <input type="text" name="{{ $name }}" class="form-control" required>
-                            @endif
-                        </div>
-                    </div>
-                    @endforeach
-
-                    <!-- Nuevo campo para la imagen de perfil -->
-                    <div class="mb-3 row">
-                        <label class="col-md-4 col-form-label text-md-end">
-                            Imagen de Perfil <span class="text-danger">*</span>
-                        </label>
-                        <div class="col-md-6">
-                            <input type="file" name="imagen_perfil" class="form-control" accept="image/*" required>
-                        </div>
-                    </div>
-
-                    <!-- Nuevo campo para Delegación -->
-                    <div class="mb-3 row">
-                        <label class="col-md-4 col-form-label text-md-end">
-                            Delegación <span class="text-danger">*</span>
-                        </label>
-                        <div class="col-md-6">
-                            <select name="DEL_id" class="form-select" required>
-                                <option value="" selected disabled>Selecciona una delegación</option>
-                                @foreach ($delegaciones as $delegacion)
-                                <option value="{{ $delegacion->DEL_id }}">{{ $delegacion->DEL_nombre }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                    </div>
-
-                    <!-- Nuevo campo para Coordinador -->
-                    <div class="mb-3 row">
-                        <label class="col-md-4 col-form-label text-md-end">
-                            Coordinador <span class="text-danger">*</span>
-                        </label>
-                        <div class="col-md-6">
-                            <select name="COO_id" class="form-select" required>
-                                <option value="" selected disabled>Selecciona un coordinador</option>
-                                @foreach ($coordinadores as $coordinador)
-                                <option value="{{ $coordinador->COO_id }}">{{ $coordinador->COO_nombre }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                    </div>
-
-                    <!-- Nuevo campo para Contraseña -->
-                    <div class="mb-3 row">
-                        <label class="col-md-4 col-form-label text-md-end">
-                            Contraseña <span class="text-danger">*</span>
-                        </label>
-                        <div class="col-md-6">
-                            <input type="password" class="form-control fs-13px" placeholder="Contraseña" name="password" required autocomplete="new-password" />
-                            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-                        </div>
-                    </div>
-
-                    <!-- Nuevo campo para Confirmar Contraseña -->
-                    <div class="mb-3 row">
-                        <label class="col-md-4 col-form-label text-md-end">
-                            Confirmar Contraseña <span class="text-danger">*</span>
-                        </label>
-                        <div class="col-md-6">
-                            <input type="password" class="form-control fs-13px" placeholder="Confirmar Contraseña" name="password_confirmation" required autocomplete="new-password" />
-                            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-                        </div>
-                    </div>
-
-                    <div class="card-footer text-end">
-                        <a href="{{ url()->previous() }}" class="btn btn-secondary">Volver</a>
-                        <button type="submit" class="btn btn-primary">Registrar</button>
-                    </div>
-            </form>
+    <div class="card shadow">
+        <div class="card-header text-white text-center">
+            <h3>Registrar Voluntario</h3>
         </div>
+        <!-- Mensajes de éxito -->
+        @if(session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+        @endif
+
+        <!-- Mensajes de error de validación -->
+        @if($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
+
+        <form action="{{ route('storeVoluntario') }}" method="POST" enctype="multipart/form-data">
+            @csrf <!-- Directiva Blade para el token CSRF -->
+            <div class="card-body">
+                <!-- Campos del formulario -->
+                @php
+                $fields = [
+                'VOL_nombre' => 'Nombre',
+                'VOL_apellidos' => 'Apellidos',
+                'VOL_dni' => 'DNI',
+                'VOL_fecha_nac' => 'Fecha de Nacimiento',
+                'VOL_domicilio' => 'Domicilio',
+                'VOL_cp' => 'Código Postal',
+                'VOL_tel1' => 'Teléfono',
+                'VOL_sexo' => 'Sexo',
+                'VOL_mail' => 'Correo Electrónico'
+                ];
+                $selectFields = [
+                'VOL_sexo' => ['Masculino', 'Femenino', 'Otro']
+                ];
+                @endphp
+
+                @foreach ($fields as $name => $label)
+                <div class="mb-3 row">
+                    <label class="col-md-4 col-form-label text-md-end">
+                        {{ $label }} <span class="text-danger">*</span>
+                    </label>
+                    <div class="col-md-6">
+                        @if (array_key_exists($name, $selectFields))
+                        <select name="{{ $name }}" class="form-select" required>
+                            <option value="" selected disabled>Selecciona una opción</option>
+                            @foreach ($selectFields[$name] as $option)
+                            <option value="{{ $option }}">{{ $option }}</option>
+                            @endforeach
+                        </select>
+                        @elseif($name == 'VOL_fecha_nac')
+                        <input type="date" name="{{ $name }}" class="form-control" required>
+                        @else
+                        <input type="text" name="{{ $name }}" class="form-control" required>
+                        @endif
+                    </div>
+                </div>
+                @endforeach
+
+                <!-- Nuevo campo para la imagen de perfil -->
+                <div class="mb-3 row">
+                    <label class="col-md-4 col-form-label text-md-end">
+                        Imagen de Perfil <span class="text-danger">*</span>
+                    </label>
+                    <div class="col-md-6">
+                        <input type="file" name="imagen_perfil" class="form-control" accept="image/*" required>
+                    </div>
+                </div>
+
+                <!-- Nuevo campo para Delegación -->
+                <div class="mb-3 row">
+                    <label class="col-md-4 col-form-label text-md-end">
+                        Delegación <span class="text-danger">*</span>
+                    </label>
+                    <div class="col-md-6">
+                        <select name="DEL_id" class="form-select" required>
+                            <option value="" selected disabled>Selecciona una delegación</option>
+                            @foreach ($delegaciones as $delegacion)
+                            <option value="{{ $delegacion->DEL_id }}">{{ $delegacion->DEL_nombre }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+
+                <!-- Nuevo campo para Coordinador -->
+                <div class="mb-3 row">
+                    <label class="col-md-4 col-form-label text-md-end">
+                        Coordinador <span class="text-danger">*</span>
+                    </label>
+                    <div class="col-md-6">
+                        <select name="COO_id" class="form-select" required>
+                            <option value="" selected disabled>Selecciona un coordinador</option>
+                            @foreach ($coordinadores as $coordinador)
+                            <option value="{{ $coordinador->COO_id }}">{{ $coordinador->COO_nombre }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+
+                <!-- Nuevo campo para Contraseña -->
+                <div class="mb-3 row">
+                    <label the class="col-md-4 col-form-label text-md-end">
+                        Contraseña <span class="text-danger">*</span>
+                    </label>
+                    <div class="col-md-6">
+                        <input type="password" class="form-control fs-13px" placeholder="Contraseña" name="password" required autocomplete="new-password" />
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                    </div>
+                </div>
+
+                <!-- Nuevo campo para Confirmar Contraseña -->
+                <div class="mb-3 row">
+                    <label the class="col-md-4 col-form-label text-md-end">
+                        Confirmar Contraseña <span class="text-danger">*</span>
+                    </label>
+                    <div class="col-md-6">
+                        <input type="password" class="form-control fs-13px" placeholder="Confirmar Contraseña" name="password_confirmation" required autocomplete="new-password" />
+                        <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-footer text-end">
+                <a href="{{ url()->previous() }}" class="btn btn-secondary">Volver</a>
+                <button type="submit" class="btn btn-primary">Registrar</button>
+            </div>
+        </form>
+    </div>
+</div>
+
         <!-- Políticas de envío o información adicional -->
         <!-- <div class="shipping-policy mt-4">
             <h5>Política de Envío</h5>
